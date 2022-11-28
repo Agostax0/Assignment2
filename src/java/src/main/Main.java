187eb2ecb8cb261f135dd0dc639361094ee21e6c
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-11-28 09:23:39 +0100

    added graphics

--- src/java/src/main/Main.java
+++ src/java/src/main/Main.java
@@ -1,16 +1,17 @@
-package main;
+package test;
 
 import java.awt.BorderLayout;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import org.jfree.*;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.Series;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
@@ -20,6 +21,7 @@ import jssc.SerialPortList;
 public class Main{
 	private static String CONNECT = "Connect";
 	private static String DISCONNECT = "Disconnect";
+	private static String FAILURE = "Failure";
 	
 	private static SerialCommChannel channel;
 
@@ -33,35 +35,64 @@ public class Main{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JComboBox<String> portList = new JComboBox<>();
-		JButton connectButton = new JButton(CONNECT);
 		
-		connectButton.addActionListener(e->{
-			JButton src = (JButton) e.getSource();
-			if(src.getText().equals(CONNECT)) {
-				
-				src.setText(DISCONNECT);
-			}
-			else {
-				
-				src.setText(CONNECT);
-			}
-		});
+		String[] ports = SerialPortList.getPortNames();
+		for(int i = 0; i < ports.length; i++) {
+			portList.addItem(ports[i]);
+		}
+		JButton connectButton = new JButton(CONNECT);
 		
 		JPanel topPanel = new JPanel();
 		topPanel.add(portList);
 		topPanel.add(connectButton);
 
 		window.add(topPanel,BorderLayout.NORTH);
-		
-		String[] ports = SerialPortList.getPortNames();
-		for(int i = 0; i < ports.length; i++) {
-			portList.addItem(ports[i]);
-		}
+
 		
 		XYSeries data = new XYSeries("Sonar Sensor Readings");
 		XYSeriesCollection dataset = new XYSeriesCollection(data);
 		
 		JFreeChart chart = ChartFactory.createXYLineChart("Water Level", "Time (seconds)" , "Height", dataset);
+		
+		connectButton.addActionListener(e->{
+			JButton src = (JButton) e.getSource();
+			if(src.getText().equals(CONNECT)) {
+				String selectedPort = portList.getSelectedItem().toString();
+				try {
+					channel = new SerialCommChannel(selectedPort,9600);
+					portList.setEnabled(false);
+					src.setText(DISCONNECT);
+				} catch (Exception e1) {
+					System.out.println("There was an error with the port");
+					System.exit(0);
+					e1.printStackTrace();
+				}
+				/*Thread thread = new Thread(){
+					int x = 0;
+					@Override
+					public void run() {
+						try {
+							String msg = channel.receiveMsg();
+							double waterLevel = Double.parseDouble(msg);
+							data.add(x++,waterLevel);
+							
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						
+					}
+				};
+				
+				thread.start();*/
+			}
+			else {
+				channel.close();
+				portList.setEnabled(true);
+				src.setText(CONNECT);
+			}
+		});
+		
+
 		
 		
 		
@@ -71,6 +102,26 @@ public class Main{
 		
 		
 		window.setVisible(true);
+		
+		int x = 0;
+		
+		while(true) {
+			if(channel!=null) {
+				try {
+					String msg = channel.receiveMsg();
+					System.out.println(msg);
+					double waterLevel = Double.parseDouble(msg);
+					data.add(x++,waterLevel);
+					
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			else {
+				System.out.println("failed to connect to port");
+			}
+			
+		}
 	}
 
 }
